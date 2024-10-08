2ac887899af097da948e66e1a5f57d722f817417
arufaki <fakihkazorla@gmail.com>
2024-10-08 19:13:24 +0700

    add: event handling practice priority task and exploration task

---
+++ 12_Event_Handling/praktikum/event-handling/src/Components/Elements/Select.jsx
@@ -0,0 +1,44 @@
+export default function Select({ ...props }) {
+    
+
+    const options = [
+        {
+            key: "Choose...",
+            value: "",
+        },
+        {
+            key: "Bag",
+            value: "bag",
+        },
+        {
+            key: "Clothes",
+            value: "clothes",
+        },
+        {
+            key: "Jeans",
+            value: "jeans",
+        },
+    ];
+
+    const handleChange = (e) => {
+        if (e.target.value == "") {
+            alert("Kau Blm pilih lagi woi🤬");
+        }
+    };
+
+    return (
+        <select
+            id="product"
+            className="bg-gray-50 border text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500 focus:outline-none"
+            onChange={handleChange}
+            required
+            {...props}
+        >
+            {options.map((option, index) => (
+                <option className="font-roboto" key={index} value={option.value}>
+                    {option.key}
+                </option>
+            ))}
+        </select>
+    );
+}
