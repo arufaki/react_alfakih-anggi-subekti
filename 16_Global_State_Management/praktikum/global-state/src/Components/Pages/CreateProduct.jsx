cbe93d26af564467fea5835503b42ec4587115a1
arufaki <fakihkazorla@gmail.com>
2024-10-18 00:39:21 +0700

    add: global state management task using redux

---
+++ 16_Global_State_Management/praktikum/global-state/src/Components/Pages/CreateProduct.jsx
@@ -0,0 +1,43 @@
+import Header from "../Fragments/Header";
+import Main from "../Layouts/Main";
+import HeroSection from "../Fragments/HeroSection";
+import Form from "../Fragments/Form";
+import Table from "../Fragments/Table";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addProduct, updateProduct } from "../../redux/productSlice";
+
+export default function CreateProduct() {
+    const [formData, setFormData] = useState(null);
+    const dataProduct = useSelector((state) => state.products.product);
+    const dispatch = useDispatch();
+
+    const handleFormSubmit = (data) => {
+        if (formData) {
+            dispatch(updateProduct({ ...formData, ...data }));
+            setFormData(null);
+        } else {
+            const newProduct = {
+                ...data,
+                id: formData ? formData.id : dataProduct.length > 0 ? dataProduct[dataProduct.length - 1].id + 1 : 1,
+            };
+
+            dispatch(addProduct(newProduct));
+        }
+    };
+
+    const handleEdit = (product) => {
+        setFormData(product);
+    };
+
+    return (
+        <>
+            <Header />
+            <Main>
+                <HeroSection />
+                <Form onSubmit={handleFormSubmit} initialData={formData} />
+                <Table datas={dataProduct} onEdit={handleEdit} />
+            </Main>
+        </>
+    );
+}
