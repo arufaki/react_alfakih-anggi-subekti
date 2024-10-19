25fb350c994bd16dc958a0000cffbff9156b8826
arufaki <fakihkazorla@gmail.com>
2024-10-19 15:56:18 +0700

    update: declare the middleware value to redux thunk

--- 16_Global_State_Management/praktikum/global-state/src/redux/store.js
+++ 16_Global_State_Management/praktikum/global-state/src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productReducer from "../redux/productSlice";
+import { thunk } from "redux-thunk";
 
 export const store = configureStore({
     reducer: {
         products: productReducer,
     },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
 });
