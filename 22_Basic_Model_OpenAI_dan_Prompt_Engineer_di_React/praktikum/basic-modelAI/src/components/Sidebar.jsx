c11fd90296637ad75b1ecb5f54da0ba20e76811e
arufaki <fakihkazorla@gmail.com>
2024-11-06 10:35:32 +0700

    update: link for navigate to the /chat endpoint

--- 22_Basic_Model_OpenAI_dan_Prompt_Engineer_di_React/praktikum/basic-modelAI/src/components/Sidebar.jsx
+++ 22_Basic_Model_OpenAI_dan_Prompt_Engineer_di_React/praktikum/basic-modelAI/src/components/Sidebar.jsx
@@ -1,12 +1,20 @@
+import { Link, useNavigate } from "react-router-dom";
+
 const Sidebar = ({ onClick, datas, handleClickHistory, response }) => {
     const historyData = datas;
     const realtimeResponse = response;
     const isResponsesEmpty = realtimeResponse.length === 0;
     return (
         <div className="h-screen w-60 bg-slate-500">
-            <button className={`mt-2 ml-3 p-3 rounded-lg cursor-pointer ${isResponsesEmpty ? "bg-gray-300" : "bg-blue-300"}`} onClick={isResponsesEmpty ? null : onClick} disabled={isResponsesEmpty}>
-                New Chat
-            </button>
+            <Link to="/chat">
+                <button
+                    className={`mt-2 ml-3 p-3 rounded-lg cursor-pointer ${isResponsesEmpty ? "bg-gray-300" : "bg-blue-300"}`}
+                    onClick={isResponsesEmpty ? null : onClick}
+                    disabled={isResponsesEmpty}
+                >
+                    New Chat
+                </button>
+            </Link>
             <div className="mt-5">
                 {historyData
                     .slice()
